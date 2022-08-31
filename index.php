ae6e0ddeec0aae752f37032e657a5f7d937d0f55
fedepaloz <palozzifederico@gmail.com>
2022-08-31 16:40:09 +0200

    nested foreach paragraphs

--- index.php
+++ index.php
@@ -2,17 +2,53 @@
 /* array di array associativi, gli array sono cinque come le faq ed al loro interno hanno i relativi paragrafi  */
 
 $faqs = [
-    "titolo primo paragrafo" => ["lorem ipsum","ciao sono federico","bella di padella","il mio nome è nessuno"],
+    "titolo primo paragrafo" => ["primo","secondo","terzo","quarto","cinque","sei"],
+    "titolo secondo paragrafo" => ["uno","due","tre","quattro"],
+    "titolo terzo paragrafo" => ["ichi","ni","san"],
 ];
 
-foreach ($faqs as $faq_paragraph) {
+/* foreach ($faqs as   $faq_paragraph) {
     foreach ($faq_paragraph as $paragraph) 
     {
         echo ($paragraph);
     }
-}
+} */
+
+
+foreach($faqs as $key => $value)
+{var_dump($key);
+var_dump($value);}
+
+
+foreach($faqs as $key => $value)
 
 
 ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <ul>
+   <?php foreach($faqs as $key => $value) : ?>
+   <li>
+    <h1> <?php echo ($key) ?> </h1>
+   </li>
+   <ul>
+    <?php foreach ($faqs as   $faq_paragraph) {
+    foreach ($faq_paragraph as $paragraph) 
+   
+     : ?>
+    <li><p> <?php echo($paragraph) ?></p></li>
+    <?php endforeach ?>
+   </ul>
+   <?php endforeach ?>
+   </ul>
+</body>
+</html>
